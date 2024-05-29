f00e9d7f73e9fbedf9c3bce93ba44e04efae142c
ayanchyaziz123 <aaiz9642@gmail.com>
2024-05-29 15:29:02 -0400

    edited

--- src/app/pages/jobs/[id]/page.js
+++ src/app/pages/jobs/[id]/page.js
@@ -1,5 +1,6 @@
 // JobDetailsPage.js
 'use client'
+// JobDetailsPage.js
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Navbar from '@/app/components/Navbar';
@@ -10,8 +11,7 @@ const JobDetailsPage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    if (router.isReady) { // Check if router is ready
-      // Fetch job details based on the ID
+    if (router.isReady) {
       const fetchJobDetails = async () => {
         try {
           const response = await fetch(`/api/jobs/${router.query.id}`);
@@ -27,20 +27,44 @@ const JobDetailsPage = () => {
 
       fetchJobDetails();
     }
-  }, [router.isReady]); // Only fetch details when router is ready
+  }, [router.isReady]);
 
   if (!job) {
-    return <p>Loading...</p>;
+    return (
+
+        <div className="container mx-auto flex flex-col min-h-screen">
+        <Navbar />
+        <div className="container mx-auto flex-grow">
+          <div className="p-4">
+            <h1 className="text-2xl font-bold">Need a bartender</h1>
+            <p className="my-4">
+              This job requires someone with experience in bartending. Responsibilities may include mixing and serving drinks,
+              interacting with customers, maintaining cleanliness and organization behind the bar, and ensuring customer satisfaction.
+              The ideal candidate should be friendly, reliable, and able to work efficiently in a fast-paced environment.
+            </p>
+            <div className="grid grid-cols-2 gap-4">
+              <p className="text-gray-600">Location: Manhattan</p>
+              <p className="text-gray-600">Wage: $18</p>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
   }
 
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
-      <div className="container mx-auto p-4">
-        <h1 className="text-2xl font-bold">{job.title}</h1>
-        <p>{job.description}</p>
-        <p>Location: {job.location}</p>
-        <p>Wage: {job.wage}</p>
+      <div className="container mx-auto flex-grow">
+        <div className="p-4">
+          <h1 className="text-2xl font-bold">{job.title}</h1>
+          <p className="my-4">{job.description}</p>
+          <div className="grid grid-cols-2 gap-4">
+            <p className="text-gray-600">Location: {job.location}</p>
+            <p className="text-gray-600">Wage: {job.wage}</p>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
---
+++ src/config/FirebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA9cxtmS8TZkNI75055A128awNLQTEpysM",
+  authDomain: "ovivabokweb.firebaseapp.com",
+  projectId: "ovivabokweb",
+  storageBucket: "ovivabokweb.appspot.com",
+  messagingSenderId: "963499703372",
+  appId: "1:963499703372:web:bb53651c705cc16856c8e9",
+  measurementId: "G-J5QFM2FEFQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
